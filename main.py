7d32679396aec16fe4947629f74e013aa4f12095
PDHS lab <pdhslab@dsusd.k12.ca.us>
2018-05-02 11:16:06 -0700

    finally got decrypting and encrypting to work, just need a while loop now

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-n = 22487
-e = 21647
-
-message = "minecraft has the best graphics"
-
 encrypted_message = ""
+decrypted_message = ""
+def encrypt1():
+  e = int(input("What is the value of e?"))
+  n = int(input("What is the value of n?"))
+  print (e,n)
+  message = input("What is the message you would like to encrypt?")
+  global encrypted_message
+  for x in message:
+      numerize = ord(x)
+      encrypt = pow(numerize, e, n)
+      denumerize = chr(encrypt)
+      encrypted_message += denumerize
+  print (encrypted_message)
 
-############ENCRYPTING###############
-for x in message:
-    
-    numerize = ord(x)
-    
-    encrypt = pow(numerize, e, n)
-    
-    denumerize = chr(encrypt)
-    
-    encrypted_message += denumerize
-    
-    print (encrypted_message)
+def decrypt():
+  d = int(input("What is the value of d?"))
+  n = int(input("What is the value of n?"))
+  print (d,n)
+  message = input("What is the message you would like to decrypt?")
+  global decrypted_message
+  for x in message:
+      numerize = ord(x)
+      decrypt = pow(numerize, d, n)
+      denumerize = chr(decrypt)
+      decrypted_message += denumerize
+  print (decrypted_message)
+decrypt()
+
+def encrypter():
+  message = input("What is the message you would like to encrypt?")
+  for x in message:
+      numerize = ord(x)
+      encrypt = pow(numerize, e, n)
+      denumerize = chr(encrypt)
+      encrypted_message += denumerize
+encrypt1()
+  
